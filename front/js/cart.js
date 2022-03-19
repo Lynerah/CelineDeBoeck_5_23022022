4f1c3e5b3c679c5754545f9aa423275613442558
Lynerah <celinedeboeck@gmail>
2022-03-19 13:42:07 +0100

    deletebutton

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,36 +1,36 @@
-
+let storedProducts = []
 loadData()
 
+
 function loadData() {
+   storedProducts = JSON.parse(localStorage.getItem("products"));
 
-   let localStorageItems = JSON.parse(localStorage.getItem("products"));
-   for (let index in localStorageItems) {
+   for (let index in storedProducts) {
 
-      fetch(`http://localhost:3000/api/products/${localStorageItems[index]._id}`)
+      fetch(`http://localhost:3000/api/products/${storedProducts[index]._id}`)
       .then(function(res) {
          if (res.ok) {
             return res.json();
          }
       })
-      .then(function(product) {
-         console.log("ok");
-         let productToShow = [];
-         productToShow.price = product.price;
-         productToShow.name = product.name;
-         productToShow.imageUrl = product.imageUrl;
-         productToShow.color = localStorageItems[index].color;
-         productToShow.quantity = localStorageItems[index].quantity;
-         showProduct(productToShow);
-         
+      .then(function(fetchedProduct) {
+
+         storedProducts[index].price = fetchedProduct.price;
+         storedProducts[index].name = fetchedProduct.name;
+         storedProducts[index].imageUrl = fetchedProduct.imageUrl;
+
+         showProduct(index);
+         addEventListenerOnButton(index);
+
       })
       .catch(function(err) {
          console.log(err);
       }); 
-
    }
 }
 
-function showProduct(data) {
+function showProduct(index) {
+  
    //CREATE <article>
    let articleCart = document.createElement("article");
    articleCart.classList.add("cart__item");
@@ -41,7 +41,7 @@ function showProduct(data) {
    let imgItem = document.createElement("img");
    cart__item__img.classList.add("cart__item__img");
    articleCart.appendChild(cart__item__img);
-   cart__item__img.appendChild(imgItem).src = `${data.imageUrl}`;
+   cart__item__img.appendChild(imgItem).src = `${storedProducts[index].imageUrl}`;
 
    //CREATE div description item
 
@@ -60,8 +60,46 @@ function showProduct(data) {
    cart__item__content__description.appendChild(colorInfoItem);
    cart__item__content__description.appendChild(priceItem);
 
-   titleCartItem.textContent = data.name;
-   priceItem.textContent = data.price;
-   colorInfoItem.textContent = data.color;
-   console.log(data.color);
+   titleCartItem.textContent = storedProducts[index].name;
+   priceItem.textContent = storedProducts[index].price+ " €";
+   colorInfoItem.textContent = storedProducts[index].color;
+
+   //CREATE block quantity
+   let content__settings = document.createElement("div");
+   let content__settings__quantity = document.createElement("div");
+   let quantityItem = `<p>Qté : ${storedProducts[index].quantity}</p>
+   <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${storedProducts[index].quantity}">`;
+
+   content__settings.classList.add("cart__item__content__settings");
+   content__settings__quantity.classList.add("cart__item__content__settings__quantity");
+
+   cart__item__content.appendChild(content__settings);
+   content__settings.appendChild(content__settings__quantity).innerHTML = `<p>Qté : ${storedProducts[index].quantity}</p>
+   <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${storedProducts[index].quantity}">`;
+
+   //CREATE div delete
+
+   let content__settings__delete = document.createElement("div");
+   content__settings__delete.classList.add("cart__item__content__settings__delete");
+   content__settings.appendChild(content__settings__delete).innerHTML = `<p class="deleteItem${index}">Supprimer</p>`
+
+
+}
+
+function totalPrice() {
+   ArrayOfPrice = [];
+   let total;
+   for(let i = 0; i< productsToShow.length; i+=1){
+      total += productsToShow[i].price * productsToShow[i].quantity
+   }
+
+}
+
+function addEventListenerOnButton(index){
+   const button = document.querySelector(".deleteItem"+index);
+   button.addEventListener("click", ()=>{
+         storedProducts.splice(index, 1);
+         localStorage.setItem("products", JSON.stringify(storedProducts));
+         location.reload();
+      });
 }
